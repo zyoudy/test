752a59c7250ac3e54bcded48454303a6cfcaa9a7
Zaid Alzyoud <31412746+zyoudy@users.noreply.github.com>
2018-11-03 04:49:03 +0200

    add the event to the text

---
+++ public/js/loginCtrl.js
@@ -0,0 +1,13 @@
+
+
+loginApp.controller('loginCtrl',['$scope','$http',($scope,$http) => {
+
+    $scope.login= () =>
+    {
+        $http.post('/api/login', {email:$scope.email , password:$scope.password}).then(r => {
+            $scope.user = r.data;
+        }, e => {
+            $scope.errorMessage = e.data.message;
+        });
+    }
+}]);
